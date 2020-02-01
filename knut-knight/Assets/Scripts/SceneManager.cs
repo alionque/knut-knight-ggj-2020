0e5fc9f3c7cfcbe43f7b0243951b92428fdebc55
Ali Onque <ali@randomwhale.com>
2020-02-01 11:13:51 -0500

    Upload some base content

---
+++ knut-knight/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionHandler : MonoBehaviour
+{
+    private static SceneTransitionHandler INSTANCE;
+
+    public static SceneTransitionHandler GetInstance() {
+        return INSTANCE;
+    }
+
+    void Awake() {
+        if (INSTANCE == null) {
+            INSTANCE = this;
+            DontDestroyOnLoad(this);
+        } else {
+            Destroy(this);
+        }
+    }
+
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadScene(int index) {
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ knut-knight/Assets/Scripts/WrenchMageInventory.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrenchMageInventory : MonoBehaviour
+{
+    private static WrenchMageInventory INSTANCE;
+
+    public WrenchMageInventory GetInstance() {
+        return INSTANCE;
+    }
+
+    private List<SpawnablePlatform> inventory = new List<SpawnablePlatform>();
+    private int currentViewingIndex = -1;
+
+    public void addItem(SpawnablePlatform platform) {
+        inventory.Add(platform);
+        currentViewingIndex = inventory.Count - 1;
+    }
+
+    public SpawnablePlatform GetSpawnablePlatform() {
+        if(currentViewingIndex == -1) {
+            return null;
+        }
+        SpawnablePlatform result = inventory[currentViewingIndex];
+        inventory.RemoveAt(currentViewingIndex);
+        return result;
+    }
+
+    public SpawnablePlatform previewSpawnablePlatform() {
+        if (currentViewingIndex == -1) {
+            return null;
+        }
+        return inventory[currentViewingIndex];
+    }
+    
+    public void moveCursorLeft() {
+        switch(currentViewingIndex) {
+            case 0: if(inventory.Count > 0) {
+                    currentViewingIndex = inventory.Count - 1;
+                } else {
+                    currentViewingIndex = -1;
+                }
+                break;
+            case -1:
+                // Do Nothing
+                break;
+            default:
+                currentViewingIndex--;
+                break;
+        }
+    }
+
+    public void moveCursorRight() {
+        if (currentViewingIndex != -1) {
+            currentViewingIndex++;
+            currentViewingIndex = currentViewingIndex % inventory.Count;
+        }
+    }
+}
