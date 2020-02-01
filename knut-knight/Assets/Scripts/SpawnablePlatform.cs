1e3597192e5e70736ed933e33348f76556563e08
Ali Onque <ali@randomwhale.com>
2020-02-01 11:37:20 -0500

    Cleaned up SpawnablePlatform class

--- knut-knight/Assets/Scripts/SpawnablePlatform.cs
+++ knut-knight/Assets/Scripts/SpawnablePlatform.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class SpawnablePlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //This is just a component to slap on a prefab to allow our code to know it is supposed to be used for spawning.
 }
